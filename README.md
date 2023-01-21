190c7c82d9d1723dceb29c34260d4903b64c1eee
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-01-21 11:40:12 +0530

    added README.md file

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# Open AI API demo App
+
+Generate an Image using Open AI API
+
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`VITE_OPEN_AI_KEY` 
+- Your open ai API key(You can find your API key at https://beta.openai.com)
+
+
+## Run Locally
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the client
+
+```bash
+  npm run dev
+```
+
+
+## Warning
+
+Do not use this app in production as anyone can access your open AI API key.
--- src/App.jsx
+++ src/App.jsx
@@ -10,35 +10,41 @@ function App() {
     "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
   );
   const configuration = new Configuration({
-    apiKey: import.meta.env.VITE_Open_AI_Key,
+    apiKey: import.meta.env.VITE_OPEN_AI_KEY,
   });
 
   const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
     setPlaceholder(`Search ${prompt}..`);
-    setLoading(true);
-    const res = await openai.createImage({
-      prompt: prompt,
-      n: 1,
-      size: "512x512",
-    });
-    setLoading(false);
-    setResult(res.data.data[0].url);
+    try {
+      setLoading(true);
+      const res = await openai.createImage({
+        prompt: prompt || placeholder,
+        n: 1,
+        size: "512x512",
+      });
+      setResult(res.data.data[0].url);
+    } catch (error) {
+      console.log("error----->", error);
+      setLoading(false);
+    }
   };
   return (
     <div className="app-main">
       {loading ? (
         <>
           <h2>Generating..Please Wait..</h2>
-          <div class="lds-ripple">
+          <div className="lds-ripple">
             <div></div>
             <div></div>
           </div>
         </>
       ) : (
         <>
-          <h2>Generate an Image using Open AI API</h2>
+          <h2 style={{ color: "black" }}>
+            Generate an Image using Open AI API
+          </h2>
 
           <textarea
             className="app-input"
@@ -47,7 +53,9 @@ function App() {
             rows="10"
             cols="40"
           />
-          <button onClick={generateImage}>Generate an Image</button>
+          <button onClick={generateImage} style={{ color: "white" }}>
+            Generate an Image
+          </button>
           {result.length > 0 ? (
             <img className="result-image" src={result} alt="result" />
           ) : (
